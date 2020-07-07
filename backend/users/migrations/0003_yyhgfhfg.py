51f87e78d839cff5c742bb715dc0413480eecc68
Crowdbotics <team@crowdbotics.com>
2020-07-07 12:20:40 +0000

    Update models

--- backend/users/migrations/0003_yyhgfhfg.py
+++ backend/users/migrations/0003_yyhgfhfg.py
@@ -6,15 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0002_auto_20200707_0815'),
+        ("users", "0002_auto_20200707_0815"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='YYhgfhfg',
+            name="YYhgfhfg",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('jhgjhgj', models.PositiveSmallIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("jhgjhgj", models.PositiveSmallIntegerField()),
             ],
         ),
     ]
